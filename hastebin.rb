dd5e49224ccbbaff7dcb880b6f51ba762c0f739f
Eckiwlco <65813001+Eckiwlco@users.noreply.github.com>
2020-05-23 18:09:29 +0530

    first push

---
+++ hastebin.rb
@@ -0,0 +1,12 @@
+require "net/http"
+require "json"
+require "uri"
+
+base = "https://hastebin.com/documents"
+uri = URI(base)
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+req = Net::HTTP::Post.new(uri.path, data: "") # you have to replace
+res = https.request(req)
+json = JSON.parse(res.body)
+puts "https://hasteb.in/#{json['key']}"
